ea0bff1af25e41aa33ead7c45471b48403e93f81
nolango <nolangonzalez@live.com>
2015-03-31 22:08:23 -0400

    something...

--- lazyme/app/controllers/refrigerators_controller.rb
+++ lazyme/app/controllers/refrigerators_controller.rb
@@ -1,2 +1,11 @@
 class RefrigeratorsController < ApplicationController
+
+def index
+  @refrigerators = Refrigerator.all
+end
+
+def show
+  @refrigerator = Refrigerator.find(params[:id])
+end
+
 end
