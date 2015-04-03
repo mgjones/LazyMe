89132891562d76db03119e68e075b7c2f724fbd2
nolango <nolangonzalez@live.com>
2015-04-02 22:56:56 -0400

    again

--- lazyme/app/controllers/refrigerators_controller.rb
+++ lazyme/app/controllers/refrigerators_controller.rb
@@ -50,23 +50,6 @@ class RefrigeratorsController < ApplicationController
 
     def show
         @refrigerator = Refrigerator.find(params[:id])
-    end
-
-    def price_range(min, max)
-        if (min == nil) and (max != nil)
-            return Refrigerator.where('price >= 0').where('price <= max')
-        elsif (min and max != nil)
-            return Refrigerator.where('min <= price').where('price <= max')
-        elsif (min and max == nil)
-            return Refrigerator.order("name")
-        else
-            return Refrigerator.order("name")
-        end
-
-        #@refrigerators
-    end
-
-    
-    
+    end    
 
 end
