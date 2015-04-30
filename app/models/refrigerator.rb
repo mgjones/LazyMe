9f041104a1626ac2c407ecb98b879f7e473c1259
nolango <nolangonzalez@live.com>
2015-04-29 23:41:57 -0400

    minor changes to accomodate new refrigerators table

--- app/models/refrigerator.rb
+++ app/models/refrigerator.rb
@@ -3,24 +3,19 @@ class Refrigerator < ActiveRecord::Base
  
   has_many :reviews
 
-  scope :price, ->(price) { where("price >= ?", price) }
-  scope :min, ->(min) { where("price >= ?", min) }
-  scope :max, ->(max) { where("price <= ?", max) }
   scope :rating, ->(min_rating) { where("rating >= ?", min_rating) }
   scope :popularity, ->(min_pop) { where("popularity >= ?", min_pop) }
-  scope :key_word, ->(key_word) { where("name like ?", "%#{key_word}%") }
   scope :brand, ->(brand) { where("brand like ?", "%#{brand}%") }
-  scope :key_features, ->(key_features) { where("keyfeatures like ?", "%#{key_features}%") }
   
   def self.sorted_by(field)
     if Refrigerator.column_names.include?(field)
       return Refrigerator.order(field)
     end
-    return Refrigerator.order("name")
+    return Refrigerator.order("brand")
   end
 
   def self.search(input)
-    return Refrigerator.where("name like ?", "%#{input}%")
+    return Refrigerator.where("brand like ?", "%#{input}%")
   end
   
   def has_reviews
