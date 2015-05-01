1a8699c52d326fbf3ae53feb61da13bbb76f8cf5
nolango <nolangonzalez@live.com>
2015-04-30 23:09:35 -0400

    added graphs to compare refrigerator details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def getFridges(myFridges)
+    product_ids = []
+    myFridges.each do |f|
+      product_ids.push(f.item_id)
+    end
+    return product_ids
+  end
+
 end
